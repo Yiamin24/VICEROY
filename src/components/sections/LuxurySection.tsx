1b6bad8321604f8cb04d1aac3fd8897d19eee451
Yiamin24 <yashsharma02412@gmail.com>
2026-03-10 14:03:12 +0530

    b down

--- src/components/sections/LuxurySection.tsx
+++ src/components/sections/LuxurySection.tsx
@@ -38,7 +38,7 @@ export default function LuxurySection() {
 
           {/* Butterfly */}
           <motion.div
-            className="absolute top-6 md:top-8 lg:top-10 right-6 md:right-10 lg:right-16"
+            className="absolute top-14 md:top-16 lg:top-20 right-6 md:right-10 lg:right-16"
             style={{ y: butterflyY }}
           >
             <img
