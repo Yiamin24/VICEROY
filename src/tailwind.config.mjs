144a41f2ec528598e7d49830b9a34c09d35bc37d
Yiamin24 <yashsharma02412@gmail.com>
2026-03-10 12:01:06 +0530

    hew

--- src/tailwind.config.mjs
+++ src/tailwind.config.mjs
@@ -19,7 +19,7 @@ export default {
                 '9xl': ['6rem', { lineHeight: '1', letterSpacing: '-0.01em', fontWeight: 'bold' }],
             },
             fontFamily: {
-                heading: ['Bodoni Moda', 'Libre Caslon Display', 'Playfair Display', 'serif'],
+                heading: ['Noe Display', 'Bodoni Moda', 'Libre Caslon Display', 'Playfair Display', 'serif'],
                 paragraph: ['Inter', 'sans-serif']
             },
             colors: {
