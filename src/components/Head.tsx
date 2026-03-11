46796a31f93c3ecaa7a1dafe53eae5de41998467
Yiamin24 <yashsharma02412@gmail.com>
2026-03-11 11:17:20 +0530

    favicon

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -4,7 +4,7 @@ export const Head = () => {
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* Favicon */}
-      <link rel="icon" type="image/svg+xml" href="https://static.wixstatic.com/shapes/cef78c_cc426ba4732a4a29b66b1529bc9c6842.svg" />
+      <link rel="icon" type="image/png" href="https://static.wixstatic.com/media/cef78c_330ef5f53e9c44b18986c54ed886b833~mv2.png" />
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
     </>
